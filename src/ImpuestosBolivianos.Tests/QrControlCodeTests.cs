bac0e09f077abf7460fe38bc0c98905683a49130
Alex Pizarro <apizarro@eventbrite.com>
2020-12-23 08:15:37 -0400

    Migrate QR control code tests

--- src/ImpuestosBolivianos.Tests/QrControlCodeTests.cs
+++ src/ImpuestosBolivianos.Tests/QrControlCodeTests.cs
@@ -1,70 +1,74 @@
 using System;
-using System.Drawing;
 using System.Globalization;
-using System.IO;
-using ZXing;
-using ZXing.Common;
-using ZXing.QrCode;
+using TinyCsvParser.Mapping;
+using TinyCsvParser.TypeConverter;
+using Xunit;
 
 namespace ImpuestosBolivianos.Tests
 {
-    /*
-    [TestClass]
     public class QrControlCodeTests
     {
-        private TestContext testContextInstance;
-        public TestContext TestContext
+        [Theory]
+        [CsvData(@"Data/QrControlCode-5000TCs.csv", typeof(TestArgs), typeof(TestArgsMapping))]
+        public void Text_InputFromQrControlCodeTCs_AllShouldPass(TestArgs t)
         {
-            get { return testContextInstance; }
-            set { testContextInstance = value; }
-        }
-
-        [TestMethod]
-        [DeploymentItem("Testcases\\QrControlCode-5000TCs.csv")]
-        [DeploymentItem("Testcases\\Schema.ini")]
-        [DataSource(
-            "Microsoft.VisualStudio.TestTools.DataSource.CSV",
-            "|DataDirectory|\\Testcases\\QrControlCode-5000TCs.csv",
-            "QrControlCode-5000TCs#csv", DataAccessMethod.Sequential
-        )]
-        public void Text_InputFromQrControlCodeTCs_AllShouldPass()
-        {
-            var invoice = CurrentInvoiceOnTestContext();
-            String expected = Convert.ToString(TestContext.DataRow["ContenidoCodigoQr"]);
-
-            var sut = new QrControlCode(invoice);
-            String actual = sut.Text;
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        private readonly CultureInfo BolivianCultureInfo = CultureInfo.CreateSpecificCulture("es-BO");
-
-        private Invoice CurrentInvoiceOnTestContext()
-        {
-            return new Invoice()
+            var invoice = new Invoice()
             {
-                NitEmisor = Convert.ToString(TestContext.DataRow["NitEmisor"]),
-                NroAutorizacion = Convert.ToInt64(TestContext.DataRow["NroAutorizacion"]),
-                NroFactura = Convert.ToInt64(TestContext.DataRow["NroFactura"]),
-                NitCliente = Convert.ToString(TestContext.DataRow["NitCliente"]),
-                CodigoControl = Convert.ToString(TestContext.DataRow["CodigoControl"]),
-                Fecha = Convert.ToDateTime(
-                    TestContext.DataRow["Fecha"], BolivianCultureInfo),
-                ImporteTotal = Convert.ToDecimal(
-                    TestContext.DataRow["ImporteTotal"], CultureInfo.InvariantCulture),
-                ImporteBaseCf = Convert.ToDecimal(
-                    TestContext.DataRow["ImporteBaseCf"], CultureInfo.InvariantCulture),
-                ImporteIceIehdTasas = Convert.ToDecimal(
-                    TestContext.DataRow["ImporteIceIehdTasas"], CultureInfo.InvariantCulture),
-                ImporteVentasNoGravadas = Convert.ToDecimal(
-                    TestContext.DataRow["ImporteVentasNoGravadas"], CultureInfo.InvariantCulture),
-                ImporteNoSujetoCf = Convert.ToDecimal(
-                    TestContext.DataRow["ImporteNoSujetoCf"], CultureInfo.InvariantCulture),
-                DescuentosBonosRebajas = Convert.ToDecimal(
-                    TestContext.DataRow["DescuentosBonosRebajas"], CultureInfo.InvariantCulture)
+                NitEmisor = t.NitEmisor,
+                NroAutorizacion = t.NroAutorizacion,
+                NroFactura = t.NroFactura,
+                NitCliente = t.NitCliente,
+                CodigoControl = t.CodigoControl,
+                Fecha = t.Fecha,
+                ImporteTotal = t.ImporteTotal,
+                ImporteBaseCf = t.ImporteBaseCf,
+                ImporteIceIehdTasas = t.ImporteIceIehdTasas,
+                ImporteVentasNoGravadas = t.ImporteVentasNoGravadas,
+                ImporteNoSujetoCf = t.ImporteNoSujetoCf,
+                DescuentosBonosRebajas = t.DescuentosBonosRebajas,
             };
+
+            string actual = new QrControlCode(invoice).Text;
+
+            Assert.Equal(t.ExpectedQrCodeContent, actual);
+        }
+
+        public class TestArgs
+        {
+            public string NitEmisor { get; set; }
+            public long NroFactura { get; set; }
+            public long NroAutorizacion { get; set; }
+            public DateTime Fecha { get; set; }
+            public decimal ImporteTotal { get; set; }
+            public decimal ImporteBaseCf { get; set; }
+            public string CodigoControl { get; set; }
+            public string NitCliente { get; set; }
+            public decimal ImporteIceIehdTasas { get; set; }
+            public decimal ImporteVentasNoGravadas { get; set; }
+            public decimal ImporteNoSujetoCf { get; set; }
+            public decimal DescuentosBonosRebajas { get; set; }
+            public string ExpectedQrCodeContent { get; set; }
+        }
+
+        public class TestArgsMapping : CsvMapping<TestArgs>
+        {
+            public TestArgsMapping()
+                : base()
+            {
+                MapProperty(1, x => x.NitEmisor);
+                MapProperty(2, x => x.NroFactura);
+                MapProperty(3, x => x.NroAutorizacion);
+                MapProperty(4, x => x.Fecha, new DateTimeConverter("dd/MM/yyyy", CultureInfo.InvariantCulture));
+                MapProperty(5, x => x.ImporteTotal, new DecimalConverter(CultureInfo.InvariantCulture));
+                MapProperty(6, x => x.ImporteBaseCf, new DecimalConverter(CultureInfo.InvariantCulture));
+                MapProperty(7, x => x.CodigoControl);
+                MapProperty(8, x => x.NitCliente);
+                MapProperty(9, x => x.ImporteIceIehdTasas, new DecimalConverter(CultureInfo.InvariantCulture));
+                MapProperty(10, x => x.ImporteVentasNoGravadas, new DecimalConverter(CultureInfo.InvariantCulture));
+                MapProperty(11, x => x.ImporteNoSujetoCf, new DecimalConverter(CultureInfo.InvariantCulture));
+                MapProperty(12, x => x.DescuentosBonosRebajas, new DecimalConverter(CultureInfo.InvariantCulture));
+                MapProperty(13, x => x.ExpectedQrCodeContent);
+            }
         }
     }
-    */
 }
