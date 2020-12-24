b7676fe6908c265bc11d85175d8143792cdfd3e1
Alex Pizarro <apizarro@eventbrite.com>
2020-12-24 17:53:15 -0400

    Add test asserting QR code PNG bytes

--- src/ImpuestosBolivianos.Tests/GenerarCodigoQrTests.cs
+++ src/ImpuestosBolivianos.Tests/GenerarCodigoQrTests.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
 using TinyCsvParser.Mapping;
 using TinyCsvParser.TypeConverter;
 using Xunit;
@@ -8,6 +12,29 @@ namespace ImpuestosBolivianos.Tests
 {
     public class GenerarCodigoQrTests
     {
+        [Fact]
+        public void GeneratesProperPngBytes()
+        {
+            var res = ImpuestosBolivianos.Facturacion.GenerarCodigoQr(
+                471625511829685L,
+                876814,
+                "7904006306693",
+                new DateTime(2008, 5, 19),
+                35958.60m,
+                "7B-F3-48-A8",
+                "1665979",
+                35958.60m,
+                0.00m,
+                0.00m,
+                0.00m,
+                0.00m);
+
+            var actualMd5 = CalculateMd5(res.BytesPng);
+
+            const string expectedMd5 = "554fb8a814402e6b6b206427452cc1a4";
+            Assert.Equal(expectedMd5, actualMd5);
+        }
+
         [Theory]
         [CsvData(@"Data/QrControlCode-5000TCs.csv", typeof(TestArgs), typeof(TestArgsMapping))]
         public void PassesTestCases(TestArgs t)
@@ -64,6 +91,19 @@ namespace ImpuestosBolivianos.Tests
                 MapProperty(11, x => x.ImporteNoSujetoCf, new DecimalConverter(CultureInfo.InvariantCulture));
                 MapProperty(12, x => x.DescuentosBonosRebajas, new DecimalConverter(CultureInfo.InvariantCulture));
                 MapProperty(13, x => x.ExpectedQrCodeContent);
+            }
+        }
+
+        private string CalculateMd5(IEnumerable<byte> bytes)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = new MemoryStream(bytes.ToArray()))
+                {
+                    var hashBytes = md5.ComputeHash(stream);
+                    var hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+                    return hash;
+                }
             }
         }
     }
